f832b696e1c2d8d270ab685ad906ba3a815233fc
bagdenia <e.ivanova@potok.digital>
2017-12-07 18:17:10 +0300

    General task done

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,21 +2,52 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Item } from 'semantic-ui-react';
 import BlogItem from 'components/widgets/blog/Item';
-import { items } from 'constants/static/items';
+import request from 'superagent';
 
-const Post = ({ match }) => {
-  const item = items[match.params.id - 1];
-  return (
-    <Item.Group>
-      <BlogItem 
-        meta = { item.meta }
-        image = { item.image }
-        text = { item.text }
-        id = {item.id }
-      />
-    </Item.Group>
-  );
-};
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { post: []};
+  }
+
+  componentDidMount() {
+    this.fetchPost(this.props.match.params.id);
+  }
+
+  fetchPost(id) {
+    request.get(
+      `http://localhost:5001/posts/${id}`,
+      {},
+      (err, res) => {
+        const item  = res.body;
+        item.meta.likes ? item.meta.likes : item.meta.likes = 0;
+        item.meta.name ? item.meta.name : item.meta.name = 'Noname'; 
+        this.setState({ post: item });
+      }
+    );
+  }
+
+  render() {
+    const item = this.state.post;
+    if (item.id) {
+      return (
+        <Item.Group>
+          <BlogItem 
+            meta = { item.meta }
+            image = { item.image }
+            text = { item.text }
+            id = {item.id }
+          />
+        </Item.Group>
+      );
+    }
+    else {
+      return (
+        <div>Not found</div>
+      );
+    }
+  }
+}
 
 Post.propTypes = {
   match: PropTypes.object
