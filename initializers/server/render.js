fc24606fec08c98bb1028646bd1fc287ef4c21ee
bagdenia <e.ivanova@potok.digital>
2018-01-24 01:26:34 +0300

    Pages 404 & 500 added

--- initializers/server/render.js
+++ initializers/server/render.js
@@ -17,39 +17,47 @@ export default (req, res) => {
   const state = { params: {}, routes: [], query: {} };
 
   // бежим по всем роутам и матчим каждый в поисках подходящих
-  routes.some(route => {
+  const matched = routes.some(route => {
     const match = matchPath(req.path, route);
     if (match)
     {
       state.routes.push(route);
       assign(state.params, match.params);
       assign(state.query, req.query);
+
+      prepareData(store, state).then(() => {
+        // и после этого рендерим страницу с начальным состоянием
+        const initialState = JSON.stringify(store.getState());
+        const content = ReactDOMServer.renderToString(
+          <Provider store={store}>
+            <StaticRouter location={req.url} context={state}>
+              <MainLayout>
+                {
+                  routes.map(
+                    (route, i) => (
+                      <RouteWithSubRoutes key={i} {...route}/>
+                    )
+                  )
+                }
+              </MainLayout>
+            </StaticRouter>
+          </Provider>
+        );
+
+        const head = Helmet.rewind();
+
+        res.status(200);
+        res.render('index', { initialState, content, head });
+      })
+        .catch(() => {
+          res.status(500);
+          res.render('500');
+        });
+      return match;
     }
-    return match;
   });
-
-  prepareData(store, state).then(() => {
-    // и после этого рендерим страницу с начальным состоянием
-    const initialState = JSON.stringify(store.getState());
-    const content = ReactDOMServer.renderToString(
-      <Provider store={store}>
-        <StaticRouter location={req.url} context={state}>
-          <MainLayout>
-            {
-              routes.map(
-                (route, i) => (
-                  <RouteWithSubRoutes key={i} {...route}/>
-                )
-              )
-            }
-          </MainLayout>
-        </StaticRouter>
-      </Provider>
-    );
-
-    const head = Helmet.rewind();
-
-    res.status(200);
-    res.render('index', { initialState, content, head });
-  });
+  if (!matched) {
+    res.status(404);
+    res.render('404');
+  }
 };
